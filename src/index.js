257418d9e425f73d515025c0584b2dc2ee03765d
xuanminh12995 <minh.vu@u.nus.edu>
2017-10-14 16:19:57 +0800

    First commit and set up react

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import ReactDOM from 'react-dom';
+import routes from './routes';
+
+/* eslint-disable */
+// Global styling
+import './theme/globalStyles';
+
+ReactDOM.render(
+  routes,
+  document.getElementById('root')
+);
